f5c52518acea16713cfce319aa8ef4aef0f405ab
hhan-solvd <130609200+hhan-solvd@users.noreply.github.com>
2023-04-20 22:03:37 -0500

    Updated Vehicle.java
    Changed the modifiers of the variables from private to protected, added the abstract method getOtherInfo() and also added equals(), hashCode() and toString().

--- cardealership/Vehicle.java
+++ cardealership/Vehicle.java
@@ -1,10 +1,12 @@
 package com.solvd.cardealership;
 
-public class Vehicle {
-    private String model;
-    private String color;
-    private int year;
-    private double price;
+import java.util.Objects;
+
+public abstract class Vehicle {
+    protected String model;
+    protected String color;
+    protected int year;
+    protected double price;
 
     public Vehicle(String vehicleModel, String vehicleColor,
                    int vehicleYear, double vehiclePrice) {
@@ -28,5 +30,33 @@ public class Vehicle {
 
     public double getPrice() {
         return price;
+    }
+
+    protected abstract String getOtherInfo();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Vehicle vehicle)) return false;
+        return getYear() == vehicle.getYear() &&
+                Double.compare(vehicle.getPrice(), getPrice()) == 0 &&
+                Objects.equals(getModel(), vehicle.getModel()) &&
+                Objects.equals(getColor(), vehicle.getColor());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getModel(), getColor(), getYear(), getPrice());
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                ", year=" + year +
+                ", price=" + price +
+                ", " + getOtherInfo() +
+                '}';
     }
 }
