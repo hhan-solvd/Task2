a7cfb9b4050d9aea549976e00d7c9ae955da8cd8
hhan-solvd <130609200+hhan-solvd@users.noreply.github.com>
2023-05-01 23:05:16 -0500

    Organized Services-related Classes in One Package
    Organized services-related classes in one package and added collections

---
+++ src/services/Finance.java
@@ -0,0 +1,46 @@
+package services;
+
+import interfaces.IPaymentMethod;
+import people.Customer;
+import vehicles.Vehicle;
+
+
+public class Finance implements IPaymentMethod {
+    private Customer customer;
+    private Vehicle vehicle;
+    private String paymentMethod;
+    private boolean needFinance = false; // if the customer needs to finance
+    private double loanAmount = 0;
+
+    public Finance(Customer buyer, Vehicle vehicleToBuy) {
+        customer = buyer;
+        vehicle = vehicleToBuy;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public double getLoanAmount() {
+        return loanAmount;
+    }
+
+    // Check if a customer needs to finance
+    public void determinePaymentMethod() {
+        if (customer.getBudget() < vehicle.getPrice()) {
+            paymentMethod = "Finance";
+            needFinance = true;
+            loanAmount = vehicle.getPrice() - customer.getBudget();
+        } else {
+            paymentMethod = "Pay in Full";
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Finance{" +
+                "needFinance=" + needFinance +
+                ", loanAmount=" + loanAmount +
+                '}';
+    }
+}
