964bd904d45985a6bb9cf25fe58b3dca35c8b232
hhan-solvd <130609200+hhan-solvd@users.noreply.github.com>
2023-05-01 22:57:16 -0500

    Organized Interfaces in One Package

---
+++ src/interfaces/IEvaluation.java
@@ -0,0 +1,9 @@
+package interfaces;
+
+public interface IEvaluation {
+    int getRating();
+
+    String getComment();
+
+    void calculateRating(); // Calculates the overall rating for an employee
+}
