2f2cf21d175871c01b60e661a8a6ce33a96bcdcd
hhan-solvd <130609200+hhan-solvd@users.noreply.github.com>
2023-05-15 17:37:12 -0500

    Added Reflection

---
+++ src/main/java/com/solvd/app/reflections/Reflection.java
@@ -0,0 +1,44 @@
+package com.solvd.app.reflections;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.lang.reflect.*;
+
+public class Reflection {
+    private static final Logger LOGGER = LogManager.getLogger(Reflection.class);
+
+    // Get and print the names, types and modifiers of fields
+    public static void printFields(Class<?> className) {
+        LOGGER.info("-------------Field List----------------");
+        Field[] fields = className.getDeclaredFields();
+        for (Field field : fields) {
+            LOGGER.info("Field: " + field.getName() +
+                    ", Type: " + field.getType() + ", " +
+                    "Modifiers: " + Modifier.toString(field.getModifiers()));
+        }
+    }
+
+    // Get and print the names, number of parameters and modifiers of constructors
+    public static void printConstructors(Class<?> className) {
+        LOGGER.info("-------------Constructor List----------------");
+        Constructor<?>[] constructors = className.getDeclaredConstructors();
+        for (Constructor<?> constructor : constructors) {
+            System.out.println("Constructor: " + constructor.getName() +
+                    ", Number of parameters: " + constructor.getParameterCount() +
+                    ", Modifiers: " + Modifier.toString(constructor.getModifiers()));
+        }
+    }
+
+    // Get and print the names, return type, number of parameters and modifiers of methods
+    public static void printMethods(Class<?> className) {
+        LOGGER.info("-------------Method List----------------");
+        Method[] methods = className.getDeclaredMethods();
+        for (Method method : methods) {
+            System.out.println("Method: " + method.getName() +
+                    ", Return type: " + method.getReturnType() +
+                    ", Number of parameters: " + method.getParameterCount() +
+                    ", Modifiers: " + Modifier.toString(method.getModifiers()));
+        }
+    }
+}
